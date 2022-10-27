da330e7ca7e5e10639e7e2bf871fdddbad2cd8ba
sniker92 <101154197+sniker92@users.noreply.github.com>
2022-10-26 21:42:47 -0400

    Add files via upload

---
+++ Hugo Massotti/Revisao.java
@@ -0,0 +1,14 @@
+public class Revisao{
+    public static void main(String[]args){
+        String numero = 1;
+        int primeiroNumero = 10;
+        int segundoNumero = 9;
+
+        int resultado = 
+            primeiroNumero + segundoNumero;
+            
+        System.out.println(resultado);
+
+
+    }
+}
